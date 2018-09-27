0b6f31e287e4999fba16d880c37f21cbe9f7a566
bhavsingh2017 <bhavkirs@usc.edu>
2018-09-27 16:54:42 -0700

    update

--- BetterMe/AppDelegate.h
+++ BetterMe/AppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by Bhav Singh on 9/27/18.
 //  Copyright © 2018 Bhav Singh. All rights reserved.
 //
-
+//I just want to see if this sends to github 
 #import <UIKit/UIKit.h>
 #import <CoreData/CoreData.h>
 
